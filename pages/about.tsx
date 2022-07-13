b1acc40d590a4604b2dd6380b36bff36fba3db1f
Tommy He <tommyhe6@gmail.com>
2022-07-13 16:41:57 -0500

    logo

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,7 @@ const about: NextPage = () => {
     return (
         <VStack>
             <Text>
-                I am a U3 B.Sc. Honors Mathematics & Computer Science student with a Cognitive Science Minor at McGill University. I am currently most interested in theoretical computer science, specifically cryptography and machine learning as well as areas of theoretical math, mostly discrete math and subfields of algebra such as algebraic number theory. I aim to take my strong math background to help me aid in studying computer science. My resume is available {" "}
+                I am a U3 B.Sc. Honors Mathematics & Computer Science student with a Cognitive Science Minor at McGill University. I am currently most interested in theoretical computer science, specifically cryptography and machine learning as well as areas of theoretical math, mostly discrete math and subfields of algebra such as algebraic number theory. I aim to take my strong math background to help gain a deeeper understanding and explore novelties in computer science. My resume is available {" "}
                 <NextLink href="Tommy_He_Resume.pdf" passHref>
                     <Link color="blue.400" isExternal>
                         here
