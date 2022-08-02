daae9dff3e309ade2070b6c4c2b56647302ba629
Tommy He <tommyhe6@gmail.com>
2022-08-01 23:43:00 -0400

    added dividers

--- components/Project.tsx
+++ components/Project.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const Project = ({ title, context, desc, time }: Props) => {
     return (
-        <VStack w="20vw" h="30vh" p={5} spacing="2vh" align="center">
+        <VStack w="20vw" h="45vh" p={5} spacing="2vh" align="center">
             <VStack spacing="0.5vh">
                 <Heading as="h3" color="redbrown" size="md" textAlign="center">
                     {title}
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Project from "../components/Project.tsx";
-import { Heading, VStack, HStack, Flex } from "@chakra-ui/react";
+import Project from "../components/Project";
+import { Heading, VStack, HStack, Flex, Divider } from "@chakra-ui/react";
 
 const projects: NextPage = () => {
     return (
@@ -10,7 +10,9 @@ const projects: NextPage = () => {
             </Heading>
             <Flex w="60vw" direction="row" justifyContent="space-between">
                 <Project title="ThreatMap" context="Hacktech top 3 ML hack" desc="Scrapes news articles to find high risk locations and levels of danger to help user safety through a website" time="May 2022" />
+                <Divider orientation="vertical"/>
                 <Project title="TikTok Dancer Trainer" context="McHacks best game dev hack" desc="Gives live feedback for similarlity between user’s dance and target dance through web app" time="Jan. 2021" />
+                <Divider orientation="vertical"/>
                 <Project title="Virtual Learning Toolbox" context="HackMIT Top 8" desc="Toolbox for improved online learning including a virtual whiteboard, gesture detection, and automatic verbal comparison" time="Sept. 2020" />
             </Flex>
         </VStack>
