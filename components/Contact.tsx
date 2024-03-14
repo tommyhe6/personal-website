3d31de75b79500c696af6dbc719b4443bd23a156
Tommy He <tommyhe6@gmail.com>
2024-03-13 23:01:00 -0700

    update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,11 +7,11 @@ import { MdEmail } from "react-icons/md";
 const Contact: () => JSX.Element = () => {
     return (
         <VStack>
-            <Text>New York City</Text>
+            <Text>San Francisco</Text>
             <HStack as="nav">
                 <NavItemIcon label="github" icon={<SiGithub />} url="https://github.com/THe142857" />
                 <NavItemIcon label="linkedin" icon={<SiLinkedin />} url="https://www.linkedin.com/in/tommyhe6/" />
-                <NavItemIcon label="email" icon={<MdEmail />} url="mailto:tommy@tommyhe.com" />
+                <NavItemIcon label="email" icon={<MdEmail />} url="mailto:tommyhe6@gmail.com" />
             </HStack>
         </VStack>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 import MyLink from "components/MyLink";
 
-import {
-  VStack,
-  Text,
-  UnorderedList,
-  ListItem,
-  Box,
-} from "@chakra-ui/react";
+import { VStack, Text, UnorderedList, ListItem, Box } from "@chakra-ui/react";
 import Head from "next/head";
 import type { NextPage } from "next";
 
@@ -22,7 +16,21 @@ const Home: NextPage = () => {
       </Head>
       <VStack alignItems="start" width="100%">
         <Box lineHeight="1.7em">
-          <Text>Hey, I&#39;m Tommy! I&#39;m currently working on <MyLink label="Clarum" url="https://clarum.ai" isExternal={true} />, bringing AI-powered due diligence to private equity firms. If you are or know anyone who is interested in improving their diligence workflow, I would love to hear from you.</Text>
+          <Text>
+            Hey, I&#39;m Tommy! I&#39;m currently working on{" "}
+            <MyLink label="Clarum" url="https://clarum.ai" isExternal={true} />,
+            bringing AI-powered due diligence to private equity firms, in the YC W24 batch.
+          </Text>
+          <Text>
+            If you are or know anyone who is interested in improving their
+            diligence workflow, I would love to{" "}
+            <MyLink
+              label="hear from you"
+              url="mailto:tommy@clarum.ai"
+              isExternal={true}
+            />
+            .
+          </Text>
         </Box>
       </VStack>
     </>
